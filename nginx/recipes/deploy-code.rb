22985ae08268be6f63571fc1eb23776069b4c930
navjotsingh-ttn <navjot.singh@tothenew.com>
2021-03-15 00:16:03 +0530

    Create deploy-code.rb

---
+++ nginx/recipes/deploy-code.rb
@@ -0,0 +1,25 @@
+execute "what am i" do
+time = Time.now.to_i
+  cwd '/tmp'
+  command "mkdir -p deploy_#{time}"
+  action :run
+end
+
+node[:deploy].each do |application, deploy|
+  if application == node[:image][:app_name]
+    if deploy[:scm]
+      s3_bucket, s3_key, base_url = OpsWorks::SCM::S3.parse_uri(deploy[:scm][:repository])
+      s3_file "/usr/share/nginx/html" do
+        bucket s3_bucket
+        remote_path s3_key
+        aws_access_key_id deploy[:scm][:user]
+        aws_secret_access_key deploy[:scm][:password]
+        owner "root"
+        group "root"
+        mode "0755"
+        s3_url base_url
+        action :create
+      end
+    end
+  end
+end
