cef8d3d4d6af099053c825e2ff684a0eaed6925e
navjotsingh-ttn <navjot.singh@tothenew.com>
2021-03-20 12:59:31 +0530

    Update install-nginx.rb

--- nginx/recipes/install-nginx.rb
+++ nginx/recipes/install-nginx.rb
@@ -1,3 +1,8 @@
+Chef::Log.info("********** For customer '#{node['customer-id']}' invoice '#{node['invoice-number']}' **********")
+Chef::Log.info("********** Invoice line number 1 is a '#{node['line-items']['line-1']}' **********")
+Chef::Log.info("********** Invoice line number 2 is a '#{node['line-items']['line-2']}' **********")
+Chef::Log.info("********** Invoice line number 3 is a '#{node['line-items']['line-3']}' **********")
+
 execute "what am i" do
 time = Time.now.to_i
   cwd '/tmp'
